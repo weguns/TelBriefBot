c41d473846ac314858c300396745723bca5a93f3
weguns <wegunss@gmail.com>
2026-02-18 16:49:55 +0330

    initial project

--- constants/API_KEYS.ts
+++ constants/API_KEYS.ts
@@ -1,6 +1,6 @@
 const API_KEYS = {
     telegramBot: "7807395877:AAE4uTLIuJc8vPlKjj_E-9MqRfSFh95YsBE",
-    metisAi: "tpsg-Q6bWdoTowgdKP3qMENQ3eXKaRnLVtVV",
+    metisAi: "tpsg-Q6bWdoTowgdKP3qMENQ3eXKatttttRnLVtVV",
 }
 
 export default API_KEYS;
